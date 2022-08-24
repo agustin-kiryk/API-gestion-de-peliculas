d421e54c322d22fa0f78568fb7d9a0cfe0553b0c
agustin <agustin.kiryk@gmail.com>
2022-08-24 09:10:40 -0300

    Initial commit

---
+++ src/test/java/com/disney/disney/DisneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disney.disney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
